ad903f5f0a55b2611cff3b4c662ed846c9d50ccc
luisfernandogaldino <133809489+luisfernandogaldino@users.noreply.github.com>
2023-10-17 16:19:12 -0300

    Add files via upload

---
+++ projetojpaUsuario3/src/main/java/com/projetojpa/ProjetojpaUsuario3Application.java
@@ -0,0 +1,13 @@
+package com.projetojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojpaUsuario3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojpaUsuario3Application.class, args);
+	}
+
+}
